cf79be1a4a4bc0817c18eccaba729e33c798e049
chenxin.shayne <chenxin.shayne@bytedance.com>
2022-02-15 14:03:38 +0800

    code for sms
    Change-Id: I84ac84e2bfc1886f257fed5a77df75d6505e8b9b

---
+++ service/sms/config.go
@@ -0,0 +1,127 @@
+package sms
+
+import (
+	"net/http"
+	"net/url"
+	"time"
+
+	"github.com/byteplus-sdk/byteplus-sdk-golang/base"
+)
+
+const (
+	DefaultRegion          = "cn-north-1"
+	ServiceVersion20200101 = "2020-01-01"
+	ServiceName            = "volcSMS"
+)
+
+var (
+	ServiceInfo = map[string]*base.ServiceInfo{
+		DefaultRegion: {
+			Timeout: 5 * time.Second,
+			Host:    "sms.volcengineapi.com",
+			Header: http.Header{
+				"Accept": []string{"application/json"},
+			},
+		},
+		base.RegionApSingapore: {
+			Timeout: 5 * time.Second,
+			Host:    "sms.byteplusapi.com",
+			Header: http.Header{
+				"Accept": []string{"application/json"},
+			},
+		},
+	}
+
+	ApiInfoList = map[string]*base.ApiInfo{
+		"SendSms": {
+			Method: http.MethodPost,
+			Path:   "/",
+			Query: url.Values{
+				"Action":  []string{"SendSms"},
+				"Version": []string{ServiceVersion20200101},
+			},
+		},
+		"SendSmsVerifyCode": {
+			Method: http.MethodPost,
+			Path:   "/",
+			Query: url.Values{
+				"Action":  []string{"SendSmsVerifyCode"},
+				"Version": []string{ServiceVersion20200101},
+			},
+		},
+		"CheckSmsVerifyCode": {
+			Method: http.MethodPost,
+			Path:   "/",
+			Query: url.Values{
+				"Action":  []string{"CheckSmsVerifyCode"},
+				"Version": []string{ServiceVersion20200101},
+			},
+		},
+		"Conversion": {
+			Method: http.MethodPost,
+			Path:   "/",
+			Query: url.Values{
+				"Action":  []string{"Conversion"},
+				"Version": []string{ServiceVersion20200101},
+			},
+		},
+	}
+)
+
+// DefaultInstance 默认的实例
+var DefaultInstance = NewInstance()
+
+// IAM .
+type SMS struct {
+	Client *base.Client
+}
+
+// NewInstance 创建一个实例
+func NewInstance() *SMS {
+	instance := &SMS{}
+	instance.Client = base.NewClient(ServiceInfo[DefaultRegion], ApiInfoList)
+	instance.Client.ServiceInfo.Credentials.Service = ServiceName
+	instance.Client.ServiceInfo.Credentials.Region = DefaultRegion
+	return instance
+}
+
+func NewInstanceI18n(region string) *SMS {
+	instance := &SMS{}
+	instance.Client = base.NewClient(ServiceInfo[base.RegionApSingapore], ApiInfoList)
+	instance.Client.ServiceInfo.Credentials.Service = ServiceName
+	instance.Client.ServiceInfo.Credentials.Region = region
+	return instance
+}
+
+// GetServiceInfo interface
+func (p *SMS) GetServiceInfo(region string) *base.ServiceInfo {
+	if serviceInfo, ok := ServiceInfo[region]; ok {
+		return serviceInfo
+	}
+	return nil
+}
+
+// GetAPIInfo interface
+func (p *SMS) GetAPIInfo(api string) *base.ApiInfo {
+	if apiInfo, ok := ApiInfoList[api]; ok {
+		return apiInfo
+	}
+	return nil
+}
+
+// SetHost .
+func (s *SMS) SetRegion(region string) {
+	if serviceInfo := s.GetServiceInfo(region); serviceInfo != nil {
+		serviceInfo.Credentials.Region = region
+	}
+}
+
+// SetHost .
+func (s *SMS) SetHost(host string) {
+	s.Client.ServiceInfo.Host = host
+}
+
+// SetSchema .
+func (s *SMS) SetSchema(schema string) {
+	s.Client.ServiceInfo.Scheme = schema
+}
---
+++ service/sms/sms_test.go
@@ -0,0 +1,102 @@
+package sms
+
+import (
+	"encoding/json"
+	"github.com/byteplus-sdk/byteplus-sdk-golang/base"
+	"testing"
+)
+
+const (
+	testAk = "testAk"
+	testSk = "testSk"
+)
+
+type Code struct {
+	Code string `json:"code"`
+}
+
+func TestSMS_send(t *testing.T) {
+	DefaultInstance.Client.SetAccessKey(testAk)
+	DefaultInstance.Client.SetSecretKey(testSk)
+	c := Code{Code: "111"}
+	cj, _ := json.Marshal(c)
+	req := &SmsRequest{
+		SmsAccount:    "smsAccount",
+		Sign:          "sign",
+		TemplateID:    "ST_xxx",
+		TemplateParam: string(cj),
+		PhoneNumbers:  "188xxxxxxxx",
+		Tag:           "tag",
+	}
+	result, statusCode, err := DefaultInstance.Send(req)
+	t.Logf("result = %+v\n", result)
+	t.Logf("statusCode = %+v\n", statusCode)
+	t.Logf("err = %+v\n", err)
+}
+
+func TestSMS_sendI18n(t *testing.T) {
+	i18nInstance := NewInstanceI18n(base.RegionApSingapore)
+	i18nInstance.Client.SetAccessKey(testAk)
+	i18nInstance.Client.SetSecretKey(testSk)
+	c := Code{Code: "111"}
+	cj, _ := json.Marshal(c)
+	req := &SmsRequest{
+		SmsAccount:    "smsAccount",
+		Sign:          "sign",
+		TemplateID:    "ST_xxx",
+		TemplateParam: string(cj),
+		PhoneNumbers:  "+65xxxxxxxx",
+		Tag:           "tag",
+	}
+	result, statusCode, err := i18nInstance.Send(req)
+	t.Logf("result = %+v\n", result)
+	t.Logf("statusCode = %+v\n", statusCode)
+	t.Logf("err = %+v\n", err)
+}
+
+func TestSMSVerifyCode_send(t *testing.T) {
+	DefaultInstance.Client.SetAccessKey(testAk)
+	DefaultInstance.Client.SetSecretKey(testSk)
+	req := &SmsVerifyCodeRequest{
+		SmsAccount:  "smsAccount",
+		Sign:        "sign",
+		TemplateID:  "ST_xxx",
+		PhoneNumber: "188xxxxxxxx",
+		Scene:       "123",
+		CodeType:    6,
+		ExpireTime:  240,
+		TryCount:    3,
+	}
+	result, statusCode, err := DefaultInstance.SendVerifyCode(req)
+	t.Logf("result = %+v\n", result)
+	t.Logf("statusCode = %+v\n", statusCode)
+	t.Logf("err = %+v\n", err)
+}
+
+func TestSMSVerifyCode_check(t *testing.T) {
+	DefaultInstance.Client.SetAccessKey(testAk)
+	DefaultInstance.Client.SetSecretKey(testSk)
+	req := &CheckSmsVerifyCodeRequest{
+		SmsAccount:  "smsAccount",
+		Scene:       "123",
+		Code:        "123456",
+		PhoneNumber: "188xxxxxxxx",
+	}
+	result, statusCode, err := DefaultInstance.CheckVerifyCode(req)
+	t.Logf("result = %+v\n", result)
+	t.Logf("statusCode = %+v\n", statusCode)
+	t.Logf("err = %+v\n", err)
+}
+
+func TestSMS_conversion(t *testing.T) {
+	client := NewInstanceI18n("ap-singapore-1")
+	client.Client.SetAccessKey(testAk)
+	client.Client.SetSecretKey(testSk)
+	req := &ConversionRequest{
+		MessageIDs: []string{"test_msg_id"},
+	}
+	result, statusCode, err := client.Conversion(req)
+	t.Logf("result = %+v\n", result)
+	t.Logf("statusCode = %+v\n", statusCode)
+	t.Logf("err = %+v\n", err)
+}
